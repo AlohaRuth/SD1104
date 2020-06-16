89b226e756f662b077e58507130a706322b15365
AlohaRuth <64048368+AlohaRuth@users.noreply.github.com>
2020-06-16 14:45:26 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Lab3_5_spamchecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lab 3_5 Spam Checker");
+            string blackListPhrase = "warrenty";
+            string blackListWords = "weight";
+            bool isSpam = false;
+            string message = Console.ReadLine();
+            if (message.Contains(blackListPhrase) || message.Contains(blackListWords))
+            {
+                isSpam = true;
+                Console.WriteLine("The message is spam");
+            }
+            
+            else
+            {
+                isSpam = false;
+
+
+                Console.WriteLine("The message is not spam");
+                
+
+                
+            }
+
+            
+
+        }
+    }
+}
